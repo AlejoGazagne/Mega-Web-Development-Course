54e88f425fda1f7ee8bdd5887ab23e725510e47e
Alejo Gazagne <gazagnealejo@gmail.com>
2024-02-25 23:34:54 -0300

    Class 88

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var price = 10;
-console.log(price)
-price = 12;
-console.log(price)
+function becomeMember(){
+    console.log("Welcome! You are a member now.")
+}
 
-const number = 20;
-console.log(number)
-number = 30;
-console.log(number)
+var num1 = prompt("Primer numero: ")
+var num2 = prompt("Segundo numero: ")
+
+function mult(n1, n2){
+    return n1*n2;
+}
+
+alert(mult(num1, num2))
