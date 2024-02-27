1bb553d226ba009d9cbfcefd1f22a6eee82b7a19
Alejo Gazagne <gazagnealejo@gmail.com>
2024-02-27 14:54:46 -0300

    Class 97

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-function becomeMember(){
-    console.log("Welcome! You are a member now.")
+function showReview(){
+    //document.querySelector(".review-con").style.opacity = "100%"
+    //document.querySelector(".product-img").style.filter = "brightness(0.6)"
+    document.querySelector(".review-con").classList.add("hover-review");
+    document.querySelector(".product-img").classList.add("hover-img");
+}
+function hideReview(){
+    //document.querySelector(".review-con").style.opacity = "0%"
+    //document.querySelector(".product-img").style.filter = "brightness(1)"
+    document.querySelector(".review-con").classList.remove("hover-review");
+    document.querySelector(".product-img").classList.remove("hover-img");
 }
 
-var num1 = prompt("Primer numero: ")
-var num2 = prompt("Segundo numero: ")
+var sun = document.getElementById("sun-icon")
+var moon = document.getElementById("moon-icon")
+var body = document.getElementsByTagName("body")[0]
+var introText = document.querySelector("#intro p")
 
-function mult(n1, n2){
-    return n1*n2;
+function darkMode(){
+    sun.style.display = "none"
+    moon.style.display = "block"
+    body.classList.toggle("body-dark")
+    introText.style.color = "#eeeff1"
 }
-
-alert(mult(num1, num2))
+function lightMode(){
+    sun.style.display = "block"
+    moon.style.display = "none"
+    body.classList.toggle("body-dark")
+    introText.style.color = "#3c404a"
+}
